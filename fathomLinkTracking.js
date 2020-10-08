b840136a0c92b22443996fabb8ebea3c34c283ae
sethl <lewandowski.seth@gmail.com>
2020-10-08 10:37:06 -0700

    added sono tracking for fathom

---
+++ fathomLinkTracking.js
@@ -0,0 +1,22 @@
+<script>
+// Prevail Tracking
+document.querySelectorAll("[href='https://www.prevail.com']").forEach(item => {
+	item.addEventListener('click', () => fathom.trackGoal('RRWE7BFH', 0));
+});
+
+// Qualis Tracking
+document.querySelectorAll("[href='https://www.qualis.com']").forEach(item => {
+	item.addEventListener('click', () => fathom.trackGoal('ZKIMOOG1', 0));
+});
+
+// PDC Tracking
+document.querySelectorAll("[href='https://www.pdcrx.com']").forEach(item => {
+	item.addEventListener('click', () => fathom.trackGoal('INZHVC6X', 0));
+});
+
+// TwinMed Tracking
+document.querySelectorAll("[href='https://www.twinmed.com']").forEach(item => {
+	item.addEventListener('click', () => fathom.trackGoal('IIA3MROJ', 0));
+});
+
+</script>
