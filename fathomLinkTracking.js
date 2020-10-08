63bd97fd6e4d934688d386c8ae0ba99dbe10bc6b
sethl <lewandowski.seth@gmail.com>
2020-10-08 11:16:08 -0700

    fixed fathom tracking script

--- fathomLinkTracking.js
+++ fathomLinkTracking.js
@@ -1,22 +1,23 @@
 <script>
 // Prevail Tracking
-document.querySelectorAll("[href='https://www.prevail.com']").forEach(item => {
+document.querySelectorAll("[href*=prevail]").forEach(item => {
 	item.addEventListener('click', () => fathom.trackGoal('RRWE7BFH', 0));
 });
 
 // Qualis Tracking
-document.querySelectorAll("[href='https://www.qualis.com']").forEach(item => {
+document.querySelectorAll("[href*=qualis]").forEach(item => {
 	item.addEventListener('click', () => fathom.trackGoal('ZKIMOOG1', 0));
 });
 
 // PDC Tracking
-document.querySelectorAll("[href='https://www.pdcrx.com']").forEach(item => {
+document.querySelectorAll("[href*='pdcrx']").forEach(item => {
 	item.addEventListener('click', () => fathom.trackGoal('INZHVC6X', 0));
 });
 
 // TwinMed Tracking
-document.querySelectorAll("[href='https://www.twinmed.com']").forEach(item => {
+document.querySelectorAll("[href*='twinmed']").forEach(item => {
 	item.addEventListener('click', () => fathom.trackGoal('IIA3MROJ', 0));
 });
 
 </script>
+
